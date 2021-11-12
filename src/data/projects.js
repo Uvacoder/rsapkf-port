37a3bf7c19949ed24cbc163e01a0779133b2de64
rsapkf <rsapkfff@pm.me>
2021-11-12 07:59:56 -0500

    refactor: fix redundant logic in /projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,94 +1,105 @@
-export const personalProjects = [
+export const projects = [
   {
-    name: "42",
-    description: "@rsapkf's linkroll.",
-    homepage: "https://links.rsapkf.xyz/",
-    repository: "https://github.com/rsapkf/42",
-    builtWith: "mdBook",
-    stargazersCount: 1900,
-    forksCount: 130,
+    label: "Personal",
+    items: [
+      {
+        name: "42",
+        description: "@rsapkf's linkroll.",
+        homepage: "https://links.rsapkf.xyz/",
+        repository: "https://github.com/rsapkf/42",
+        builtWith: "mdBook",
+        stargazersCount: 1900,
+        forksCount: 130,
+      },
+      {
+        name: "73",
+        description: "@rsapkf's notes.",
+        homepage: "https://notes.rsapkf.xyz/",
+        repository: "https://github.com/rsapkf/73",
+        builtWith: "Docusaurus, Algolia DocSearch",
+        stargazersCount: 6,
+        forksCount: 0,
+      },
+      {
+        name: "www",
+        description: "@rsapkf's homepage.",
+        homepage: "https://rsapkf.xyz/",
+        repository: "https://github.com/rsapkf/www",
+        builtWith: "Gatsby",
+        stargazersCount: 4,
+        forksCount: 1,
+      },
+      {
+        name: "dotfiles",
+        description: "@rsapkf's dotfiles.",
+        repository: "https://github.com/rsapkf/dotfiles",
+        stargazersCount: 1,
+        forksCount: 0,
+      },
+    ],
   },
   {
-    name: "73",
-    description: "@rsapkf's notes.",
-    homepage: "https://notes.rsapkf.xyz/",
-    repository: "https://github.com/rsapkf/73",
-    builtWith: "Docusaurus, Algolia DocSearch",
-    stargazersCount: 6,
-    forksCount: 0,
+    label: "Software",
+    items: [
+      {
+        name: "LFP Library",
+        description:
+          "Browse the Lex Fridman Podcast with stats, categories and books mentioned on the episodes.",
+        homepage: "https://lfplibrary.netlify.app/",
+        builtWith:
+          "Gatsby, TailwindCSS, Algolia, React Table, styled-components, Chart.js",
+        isPrivate: true,
+      },
+      {
+        name: "GNU Readline library reference",
+        description:
+          "Comprehensive GNU Readline shortcuts guide for vi + Emacs editing modes.",
+        homepage: "https://rsapkf.github.io/gnureadline-reference/",
+        repository: "https://github.com/rsapkf/gnureadline-reference",
+        builtWith: "Bootstrap, Handlebars",
+        stargazersCount: 4,
+        forksCount: 0,
+      },
+      {
+        name: "Inviduration",
+        description:
+          "Firefox add-on to view total duration of playlists on Invidio.us instances.",
+        homepage:
+          "https://addons.mozilla.org/en-US/firefox/addon/inviduration/",
+        repository: "https://github.com/rsapkf/inviduration",
+        stargazersCount: 1,
+        forksCount: 0,
+      },
+      {
+        name: "tzinfo",
+        description:
+          "Check and compare local times of major cities of the world.",
+        homepage: "https://rsapkf.github.io/tzinfo/",
+        repository: "https://github.com/rsapkf/tzinfo",
+        builtWith: "Day.js, Bulma",
+        stargazersCount: 1,
+        forksCount: 0,
+      },
+    ],
   },
   {
-    name: "www",
-    description: "@rsapkf's homepage.",
-    homepage: "https://rsapkf.xyz/",
-    repository: "https://github.com/rsapkf/www",
-    builtWith: "Gatsby",
-    stargazersCount: 4,
-    forksCount: 1,
-  },
-  {
-    name: "dotfiles",
-    description: "@rsapkf's dotfiles.",
-    repository: "https://github.com/rsapkf/dotfiles",
-    stargazersCount: 1,
-    forksCount: 0,
-  },
-]
-
-export const softwareProjects = [
-  {
-    name: "LFP Library",
-    description:
-      "Browse the Lex Fridman Podcast with stats, categories and books mentioned on the episodes.",
-    homepage: "https://lfplibrary.netlify.app/",
-    builtWith:
-      "Gatsby, TailwindCSS, Algolia, React Table, styled-components, Chart.js",
-    isPrivate: true,
-  },
-  {
-    name: "GNU Readline library reference",
-    description:
-      "Comprehensive GNU Readline shortcuts guide for vi + Emacs editing modes.",
-    homepage: "https://rsapkf.github.io/gnureadline-reference/",
-    repository: "https://github.com/rsapkf/gnureadline-reference",
-    builtWith: "Bootstrap, Handlebars",
-    stargazersCount: 4,
-    forksCount: 0,
-  },
-  {
-    name: "Inviduration",
-    description:
-      "Firefox add-on to view total duration of playlists on Invidio.us instances.",
-    homepage: "https://addons.mozilla.org/en-US/firefox/addon/inviduration/",
-    repository: "https://github.com/rsapkf/inviduration",
-    stargazersCount: 1,
-    forksCount: 0,
-  },
-  {
-    name: "tzinfo",
-    description: "Check and compare local times of major cities of the world.",
-    homepage: "https://rsapkf.github.io/tzinfo/",
-    repository: "https://github.com/rsapkf/tzinfo",
-    builtWith: "Day.js, Bulma",
-    stargazersCount: 1,
-    forksCount: 0,
-  },
-]
-
-export const miscProjects = [
-  {
-    name: "rupiks",
-    description: "SVG and PNG visualizations for Rubik's Cube algs.",
-    repository: "https://github.com/rsapkf/rupiks",
-    builtWith: "VisualCube",
-    stargazersCount: 0,
-    forksCount: 0,
-  },
-  {
-    name: "wallpapers",
-    description: "@rsapkf's wallpapers.",
-    repository: "https://github.com/rsapkf/wallpapers",
-    stargazersCount: 1,
-    forksCount: 0,
+    label: "Miscellaneous",
+    items: [
+      {
+        name: "rupiks",
+        description: "SVG and PNG visualizations for Rubik's Cube algs.",
+        repository: "https://github.com/rsapkf/rupiks",
+        builtWith: "VisualCube",
+        stargazersCount: 0,
+        forksCount: 0,
+      },
+      {
+        name: "wallpapers",
+        description: "@rsapkf's wallpapers.",
+        repository: "https://github.com/rsapkf/wallpapers",
+        stargazersCount: 1,
+        forksCount: 0,
+      },
+    ],
   },
 ]

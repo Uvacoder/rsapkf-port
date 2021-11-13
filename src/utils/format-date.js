477ddcb77c7cec824689b0ea2bacc15c69a2c2f1
rsapkf <rsapkfff@pm.me>
2021-11-12 23:02:22 -0500

    fix: use correct date format option

--- src/utils/format-date.js
+++ src/utils/format-date.js
@@ -8,7 +8,7 @@ export const formatDate = dateString => {
     hour: "2-digit",
     minute: "2-digit",
     second: "2-digit",
-    hour12: "false",
+    hour12: false,
   }
   return new Date(dateString).toLocaleDateString("en-US", options)
 }

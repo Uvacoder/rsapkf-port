9fb5cd666683decf127a5130e51f721f0ab72217
rsapkf <rsapkfff@pm.me>
2021-12-09 11:37:36 -0500

    fix: add timeout to Mastodon API request

--- src/services/mastodon-api.js
+++ src/services/mastodon-api.js
@@ -5,7 +5,11 @@ const baseUrl = 'https://mastodon.social/api/v1/'
 
 export const fetchData = async () => {
   try {
-    const result = await axios.get(`${baseUrl}accounts/${MASTODON_ID}/statuses`)
+    const result = await axios({
+      method: 'get',
+      url: `${baseUrl}accounts/${MASTODON_ID}/statuses`,
+      timeout: 3000,
+    })
     return result.data
   } catch (error) {
     return error.message
